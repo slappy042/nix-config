7806d3da18bb37fba056d0cc4eab72ac825f1000
slappy042 <75365007+slappy042@users.noreply.github.com>
2025-06-23 19:13:32 -0700

    push a bunch fixes to sops, ssh, vscode_host, etc

---
+++ home/jeff/common/core/vscode_host.nix
@@ -0,0 +1,6 @@
+{
+  # This is a fix to enable VSCode to successfully remote SSH on a client to a NixOS host
+  # https://wiki.nixos.org/wiki/Visual_Studio_Code # Remote_SSH
+  programs.nix-ld.enable = true;
+  programs.nix-ld.package = pkgs.nix-ld-rs;
+}
