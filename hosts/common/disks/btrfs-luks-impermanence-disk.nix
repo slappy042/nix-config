9d25140d18cafd7aa2790a8e550fc263d2d53d2b
slappy042 <75365007+slappy042@users.noreply.github.com>
2025-06-23 21:10:03 -0700

    disable yubikey for luks

--- hosts/common/disks/btrfs-luks-impermanence-disk.nix
+++ hosts/common/disks/btrfs-luks-impermanence-disk.nix
@@ -37,11 +37,6 @@
                 passwordFile = "/tmp/disko-password"; # this is populated by bootstrap-nixos.sh
                 settings = {
                   allowDiscards = true;
-                  # https://github.com/hmajid2301/dotfiles/blob/a0b511c79b11d9b4afe2a5e2b7eedb2af23e288f/systems/x86_64-linux/framework/disks.nix#L36
-                  crypttabExtraOpts = [
-                    "fido2-device=auto"
-                    "token-timeout=10"
-                  ];
                 };
                 # Subvolumes must set a mountpoint in order to be mounted,
                 # unless their parent is mounted
@@ -83,8 +78,4 @@
       };
     };
   };
-
-  environment.systemPackages = [
-    pkgs.yubikey-manager # For luks fido2 enrollment before full install
-  ];
 }
