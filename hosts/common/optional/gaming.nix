641dcde93479145a5c40e48d785318854076b656
slappy042 <75365007+slappy042@users.noreply.github.com>
2025-06-25 01:38:36 -0700

    steam proton

--- hosts/common/optional/gaming.nix
+++ hosts/common/optional/gaming.nix
@@ -63,4 +63,14 @@
       };
     };
   };
+
+  # https://journix.dev/posts/gaming-on-nixos/
+  environment.systemPackages = with pkgs; [
+    protonup
+  ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+
 }
