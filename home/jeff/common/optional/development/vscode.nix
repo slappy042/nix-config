838acc3bd45f4b6f75a295d269a1ada3487078e2
slappy042 <75365007+slappy042@users.noreply.github.com>
2025-06-25 01:37:28 -0700

    vscode installed but no extensions

--- home/jeff/common/optional/development/vscode.nix
+++ home/jeff/common/optional/development/vscode.nix
@@ -1,66 +1,76 @@
 # Visual Studio Code with extensions
 
-{ pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  # Get marketplace extensions from input (they should inherit allowUnfree through overlay)
+  vscode-marketplace = pkgs.nix-vscode-extensions.vscode-marketplace;
+in
 {
   programs.vscode = {
     enable = true;
-    extensions =
-      with pkgs.vscode-extensions;
-      [
-        # Language support
-        ms-python.python
-        ms-vscode.cpptools
-        # ms-dotnettools.csharp
-        # golang.go
-        # rust-lang.rust-analyzer
-        # bradlc.vscode-tailwindcss
+    # profiles.default = {
+    #   extensions = with vscode-marketplace; [
+    #     # Language support
+    #     ms-python.python
+    #     ms-vscode.cpptools
+    #     # ms-dotnettools.csharp
+    #     # golang.go
+    #     # rust-lang.rust-analyzer
+    #     # bradlc.vscode-tailwindcss
+    #     ms-azuretools.vscode-docker
 
-        # Nix support
-        bbenoist.nix
-        jnoortheen.nix-ide
+    #     # Nix support
+    #     bbenoist.nix
+    #     jnoortheen.nix-ide
 
-        # Git
-        eamodio.gitlens
-        github.vscode-pull-request-github
+    #     # Git
+    #     eamodio.gitlens
+    #     github.vscode-pull-request-github
 
-        # Productivity
-        ms-vscode.hexeditor
-        # ms-vsliveshare.vsliveshare
-        esbenp.prettier-vscode
-        # ms-vscode.vscode-json
-        redhat.vscode-yaml
-        ms-vscode-remote.remote-ssh
-        ms-vscode.remote-explorer
+    #     # Productivity
+    #     ms-vscode.hexeditor
+    #     # ms-vsliveshare.vsliveshare
+    #     esbenp.prettier-vscode
+    #     redhat.vscode-yaml
+    #     ms-vscode-remote.remote-ssh
+    #     # ms-vscode.remote-explorer
 
-        # Themes
-        pkief.material-icon-theme
-        zhuangtongfa.material-theme
+    #     # Themes
+    #     pkief.material-icon-theme
+    #     zhuangtongfa.material-theme
 
-        # Additional extensions
-        johnpapa.vscode-peacock
-        tomoki1207.pdf
+    #     # Additional extensions
+    #     johnpapa.vscode-peacock
+    #     tomoki1207.pdf
+    #     github.copilot
 
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        # Additional extensions from marketplace
-        {
-          name = "copilot";
-          publisher = "github";
-          version = "1.138.0";
-          sha256 = "sha256-/U1s1ceUm7/qFjjYgJH2QwZQ8l3/Z9XAQX+FY+L6v6g=";
-        }
-      ];
+    #   ];
+    #   # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    #   #     # Additional extensions from marketplace
+    #   #     {
+    #   #     name = "copilot";
+    #   #     publisher = "github";
+    #   #     version = "1.138.0";
+    #   #     sha256 = "sha256-/U1s1ceUm7/qFjjYgJH2QwZQ8l3/Z9XAQX+FY+L6v6g=";
+    #   #     }
+    #   # ];
 
-    userSettings = {
-      "editor.fontFamily" = "'Fira Code', 'monospace'";
-      "editor.fontLigatures" = true;
-      "editor.fontSize" = 14;
-      "editor.tabSize" = 2;
-      "editor.insertSpaces" = true;
-      "editor.formatOnSave" = true;
-      "files.autoSave" = "afterDelay";
-      "workbench.colorTheme" = "Material Theme";
-      "workbench.iconTheme" = "material-icon-theme";
-    };
+    #   userSettings = {
+    #     "editor.fontFamily" = "'Fira Code', 'monospace'";
+    #     "editor.fontLigatures" = true;
+    #     "editor.fontSize" = 14;
+    #     "editor.tabSize" = 2;
+    #     "editor.insertSpaces" = true;
+    #     "editor.formatOnSave" = true;
+    #     "files.autoSave" = "afterDelay";
+    #     "workbench.colorTheme" = "Material Theme";
+    #     "workbench.iconTheme" = "material-icon-theme";
+    #   };
+    # };
   };
 }
