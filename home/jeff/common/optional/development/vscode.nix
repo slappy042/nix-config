9be2ef8f137e0da8b1e81d2b56f30f708d41e2c4
slappy042 <75365007+slappy042@users.noreply.github.com>
2025-06-29 12:41:22 -0700

    vscode should sync settings

--- home/jeff/common/optional/development/vscode.nix
+++ home/jeff/common/optional/development/vscode.nix
@@ -13,64 +13,80 @@ in
 {
   programs.vscode = {
     enable = true;
-    # profiles.default = {
-    #   extensions = with vscode-marketplace; [
-    #     # Language support
-    #     ms-python.python
-    #     ms-vscode.cpptools
-    #     # ms-dotnettools.csharp
-    #     # golang.go
-    #     # rust-lang.rust-analyzer
-    #     # bradlc.vscode-tailwindcss
-    #     ms-azuretools.vscode-docker
+    profiles.default = {
+      extensions = with vscode-marketplace; [
+        # Language support
+        ms-python.python
+        ms-vscode.cpptools
+        # ms-dotnettools.csharp
+        # golang.go
+        # rust-lang.rust-analyzer
+        # bradlc.vscode-tailwindcss
+        ms-azuretools.vscode-docker
 
-    #     # Nix support
-    #     bbenoist.nix
-    #     jnoortheen.nix-ide
+        # Nix support
+        bbenoist.nix
+        jnoortheen.nix-ide
 
-    #     # Git
-    #     eamodio.gitlens
-    #     github.vscode-pull-request-github
+        # Git
+        eamodio.gitlens
+        github.vscode-pull-request-github
+        github.github-vscode-theme
 
-    #     # Productivity
-    #     ms-vscode.hexeditor
-    #     # ms-vsliveshare.vsliveshare
-    #     esbenp.prettier-vscode
-    #     redhat.vscode-yaml
-    #     ms-vscode-remote.remote-ssh
-    #     # ms-vscode.remote-explorer
+        # Productivity
+        ms-vscode.hexeditor
+        # ms-vsliveshare.vsliveshare
+        esbenp.prettier-vscode
+        redhat.vscode-yaml
+        ms-vscode-remote.remote-ssh
+        # ms-vscode.remote-explorer
 
-    #     # Themes
-    #     pkief.material-icon-theme
-    #     zhuangtongfa.material-theme
+        # Themes
+        pkief.material-icon-theme
+        zhuangtongfa.material-theme
 
-    #     # Additional extensions
-    #     johnpapa.vscode-peacock
-    #     tomoki1207.pdf
-    #     github.copilot
+        # Additional extensions
+        johnpapa.vscode-peacock
+        tomoki1207.pdf
+        github.copilot
 
-    #   ];
-    #   # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-    #   #     # Additional extensions from marketplace
-    #   #     {
-    #   #     name = "copilot";
-    #   #     publisher = "github";
-    #   #     version = "1.138.0";
-    #   #     sha256 = "sha256-/U1s1ceUm7/qFjjYgJH2QwZQ8l3/Z9XAQX+FY+L6v6g=";
-    #   #     }
-    #   # ];
+      ];
+      #   # ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      #   #     # Additional extensions from marketplace
+      #   #     {
+      #   #     name = "copilot";
+      #   #     publisher = "github";
+      #   #     version = "1.138.0";
+      #   #     sha256 = "sha256-/U1s1ceUm7/qFjjYgJH2QwZQ8l3/Z9XAQX+FY+L6v6g=";
+      #   #     }
+      #   # ];
+      #   userSettings = {
+      #     "editor.fontFamily" = "'Fira Code', 'monospace'";
+      #     "editor.fontLigatures" = true;
+      #     "editor.fontSize" = 14;
+      #     "editor.tabSize" = 2;
+      #     "editor.insertSpaces" = true;
+      #     "editor.formatOnSave" = true;
+      #     "files.autoSave" = "afterDelay";
+      #     "workbench.colorTheme" = "Material Theme";
+      #     "workbench.iconTheme" = "material-icon-theme";
+      #   };
+      # };
 
-    #   userSettings = {
-    #     "editor.fontFamily" = "'Fira Code', 'monospace'";
-    #     "editor.fontLigatures" = true;
-    #     "editor.fontSize" = 14;
-    #     "editor.tabSize" = 2;
-    #     "editor.insertSpaces" = true;
-    #     "editor.formatOnSave" = true;
-    #     "files.autoSave" = "afterDelay";
-    #     "workbench.colorTheme" = "Material Theme";
-    #     "workbench.iconTheme" = "material-icon-theme";
-    #   };
-    # };
+      userSettings = {
+        # Settings Sync configuration (these can't be synced themselves)
+        "settingsSync.account" = "slappy042";
+        "settingsSync.enable" = true;
+        "settingsSync.ignoredSettings" = [
+          "git.path" # Environment-specific, managed by Nix
+        ];
+        "settingsSync.ignoredExtensions" = [ ];
+        "settingsSync.keybindingsPerPlatform" = false;
+
+        # Environment-specific settings (managed by Nix, not synced)
+        "git.path" = "${pkgs.git}/bin/git";
+        "git.useConfigOnly" = true;
+      };
+    };
   };
 }
