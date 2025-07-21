770b928ae625eb32508517baf9b1d99ae0ba2efa
slappy042 <75365007+slappy042@users.noreply.github.com>
2025-07-20 18:16:23 -0700

    z13: enable bluetooth

--- hosts/nixos/z13flow/default.nix
+++ hosts/nixos/z13flow/default.nix
@@ -43,6 +43,7 @@
       # ========== Optional Configs ==========
       #
       # "hosts/common/optional/services/greetd.nix" # display manager
+      "hosts/common/optional/services/bluetooth.nix" # Bluetooth
       "hosts/common/optional/services/openssh.nix" # allow remote SSH access
       "hosts/common/optional/services/printing.nix" # CUPS
       "hosts/common/optional/audio.nix" # pipewire and cli controls
